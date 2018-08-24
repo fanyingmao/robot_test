2d06797206335174ded1f232986db9cac0bf808f
fanyingmao <646900491@qq.com>
2018-08-24 19:06:49 +0800

    test

--- app/script/lord.js
+++ app/script/lord.js
@@ -2,7 +2,7 @@
 var cwd = process.cwd();
 var mjplayer = require(cwd + "/app/script/mjplayer");
 
-var starTime = Math.random() * 10 * 1000;
+var starTime = Math.random() * 20 * 1000;
 console.log("starTime = "+ starTime);
 setTimeout(function () {
     mjplayer.guest();
--- run2.sh
+++ run2.sh
@@ -3,4 +3,4 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm use v6.12.2
 
-for i in {1..200}; do (node app.js client &);sleep 0.2; done
+for i in {1..90}; do (node app.js client &);sleep 0.2; done
